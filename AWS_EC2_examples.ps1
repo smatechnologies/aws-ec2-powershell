2b9b92a284257fbc55e2d743d81e606245e716f1
Bruce Jernell <59374529+opconWizard@users.noreply.github.com>
2020-01-10 14:07:18 -0500

    Add files via upload

---
+++ AWS_EC2_examples.ps1
@@ -0,0 +1,315 @@
+﻿param(
+    $accesskey,
+    $secretkey,
+    $profile = "default",
+    $imagename = "WINDOWS_2019_BASE",
+    $imageid,
+    $instancetype = "t2.micro", 
+    $region = "us-west-2",
+    $tag,
+    $option,
+    $apitoken,
+    $apiaddress,
+    $agentname,
+    $agentsocket = "3100",
+    $agentdescription = "AWS Test Instance",
+    $agenttype ="Windows",
+    $agentjors = "3110",
+    $groupName = "default",
+    $ip,
+    $imagedescription = "AWS Image",
+    $port,
+    $ipDescription,
+    $keyname = "Demo",
+    $opconmodule = "C:\ProgramData\OpConxps\Demo\OpCon.psm1",
+    $awsmodule = "C:\ProgramData\OpConxps\Demo\AWS.psm1",
+    $volumetype = "gp2",
+    $volumesize = 30
+)
+
+if(Test-Path $awsmodule)
+{
+    #Import needed modules
+    try
+    {
+        Import-Module AWSPowershell
+        Import-Module -Name $awsmodule -Force
+
+        #Set-DefaultAWSRegion -Region $region
+
+        Get-AWSPowerShellVersion #Output version
+    }
+    catch [Exception]
+    {
+        Write-Host "AWS Powershell modules not found!"
+        Exit 100
+    }
+}
+else
+{
+    Write-Host "Unable to import AWS modules!"
+    Exit 100
+}
+
+#Call various functions based on option input as parameter
+if($option -eq "createAWSOPCON")
+{
+    if(Test-Path $opconmodule)
+    {
+        Import-Module -Name $opconmodule -Force
+    }
+    else
+    {
+        Write-Host "Unable to import OpCon modules!"
+        Exit 100        
+    }
+
+    SMA_CreateAWSInstance -awsimageid $imageid -awsimagename $imagename -awsinstancetype $instancetype -tagvalue $tag -key $keyname -region $region
+
+    $counter = 0
+    $status = ""
+    While((($status.InstanceState.Name.Value -ne "running") -or ($status.SystemStatus.Status.Value -ne "ok") -or ($status.Status.Status.Value -ne "ok")) -and ($counter -lt 100))
+    {
+        try
+        {
+            $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region | Where-Object{$_.State.Name.Value -eq "running"}
+        
+            if($getinfo)
+            {
+                $status = (Get-EC2InstanceStatus -InstanceId $getinfo.InstanceId -region $region)
+            }
+        }
+        catch [Exception]
+        {
+            Write-Host $_.Exception.Message
+            Exit 103
+        }
+
+        Start-Sleep -s 6
+        $counter++
+    }
+
+    if($counter -gt 99)
+    {
+        Write-Host "Instance took too long to start"
+        Exit 105
+    }
+
+    $getinfo
+    Write-Host "Number of loops: $counter`r`n"
+
+    SMA_CreateAgent -agentname $tag -agenttype $agenttype -agentdescription $agentdescription -agentsocket $agentsocket -agentjors $agentjors -token $apitoken -url $apiaddress
+    SMA_UpdateAgent -agentname $tag -url $apiaddress -token $apitoken -field "tcpIpAddress" -value $getinfo.PublicIpAddress
+    SMA_UpdateAgent -agentname $tag -url $apiaddress -token $apitoken -field "fileTransferRole" -value "T"
+    SMA_UpdateAgent -agentname $tag -url $apiaddress -token $apitoken -field "fileTransferPortNumberForNonTLS" -value $agentjors
+    SMA_UpdateAgent -agentname $tag -url $apiaddress -token $apitoken -field "supportNonTLSForSMAFTServer" -value "true"
+    SMA_UpdateAgent -agentname $tag -url $apiaddress -token $apitoken -field "supportNonTLSForSMAFTAgent" -value "true"
+}
+elseif($option -eq "getAWSIPforOpCon")
+{
+    if(Test-Path $opconmodule)
+    {
+        Import-Module -Name $opconmodule -Force
+    }
+    else
+    {
+        Write-Host "Unable to import OpCon modules!"
+        Exit 100        
+    }
+
+    if($agentname)
+    { $tag = $agentname }
+
+    $aws = (SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region).PublicIpAddress
+    SMA_UpdateAgent -agentname $tag -url $apiaddress -token $apitoken -field "tcpIpAddress" -value $aws
+    Start-Sleep -Seconds 2
+
+    $machine = SMA_GetAgent -agentname "$tag" -url $apiaddress -token $apitoken
+    $machine[0].availableProperties = $machine[0].availableProperties -eq 0
+    $machine[0].availableProperties += ,@(@{name="IP";value=$aws})
+    SMA_UpdateAgent -agentname $tag -token $apitoken -url $apiaddress -field "availableProperties" -value $machine[0].availableProperties
+}
+elseif($option -eq "createinstance")
+{
+    $awsServer = SMA_CreateAWSInstance -awsimageid $imageid -awsimagename $imagename -awsinstancetype $instancetype -tagvalue $tag -key $keyname
+
+    $counter = 0
+    $status = ""
+    While(($status -ne "running") -and ($counter -lt 20))
+    {
+        $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+        $status = $getinfo.State.Name.Value
+
+        Start-Sleep -Seconds 30
+        $counter++
+    }
+
+    $getinfo
+
+    if($counter -eq 20)
+    {
+        Write-Host "Instance took too long to start"
+        Exit 105
+    }
+}
+elseif($option -eq "createBlankInstance")
+{
+    $awsServer = SMA_CreateAWSInstance -awsimageid $imageid -awsimagename $imagename -awsinstancetype $instancetype -tagvalue $tag -key $keyname -isblank "yes"
+
+    $counter = 0
+    $status = ""
+    While(($status -ne "running") -and ($counter -lt 20))
+    {
+        $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+        $status = $getinfo.State.Name.Value
+
+        Start-Sleep -Seconds 30
+        $counter++
+    }
+
+    $getinfo
+
+    if($counter -eq 20)
+    {
+        Write-Host "Instance took too long to start"
+        Exit 105
+    }
+}
+elseif($option -eq "stopinstance")
+{ 
+    $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+    $awsServer = SMA_StopAWSInstance -id $getinfo.InstanceId -region $region
+
+    $counter = 0
+    $status = ""
+    While(($status -ne "stopped") -and ($counter -lt 40))
+    {
+        $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+        $status = $getinfo.State.Name.Value
+
+        Start-Sleep -Seconds 15
+        $counter++
+    }
+
+    $getinfo
+
+    if($counter -eq 40)
+    {
+        Write-Host "Instance took too long to stop"
+        Exit 105
+    }
+}
+elseif($option -eq "startinstance")
+{ 
+    $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+    $awsServer = SMA_StartAWSInstance -id $getinfo.InstanceId -region $region
+
+    $counter = 0
+    $status = ""
+    While(($status -ne "running") -and ($counter -lt 40))
+    {
+        $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+        $status = $getinfo.State.Name.Value
+
+        Start-Sleep -Seconds 15
+        $counter++
+    }
+
+    $getinfo
+
+    if($counter -eq 40)
+    {
+        Write-Host "Instance took too long to start"
+        Exit 105
+    }
+}
+elseif($option -eq "removeInstance")
+{
+    $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+    $remove = SMA_RemoveAWSInstance -id $getinfo.InstanceId -region $region
+
+    $counter = 0
+    $status = ""
+    While(($status -ne "terminated") -and ($counter -lt 10))
+    {
+        $getinfo = SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region
+        $status = $getinfo.State.Name.Value
+
+        Start-Sleep -Seconds 30
+        $counter++
+    }
+
+    $getinfo
+
+    if($counter -eq 10)
+    {
+        Write-Host "Instance removal took too long"
+        Exit 105
+    }
+}
+elseif($option -eq "setupProfile")
+{
+    SMA_SetupAWSProfile -accesskey $accesskey -secretkey $secretkey -profile $profile
+}
+elseif($option -eq "verifyProfile")
+{
+    SMA_GetAWSProfiles
+}
+elseif($option -eq "createImage")
+{
+    $image = SMA_CreateAWSImage -instancetag $tag -imagename $imagename -imagedescription $imagedescription -region $region
+    $image
+
+    $counter = 0
+    $status = ""
+    While(($status -ne "available") -and ($counter -lt 40))
+    {
+        $status = (Get-EC2Image -ImageId $image.ImageId -Region $region).State
+
+        Start-Sleep -Seconds 15
+        $counter++
+    }
+
+    if($counter -eq 40)
+    {
+        Write-Host "Image creation took too long"
+        Exit 105
+    }
+}
+elseif($option -eq "addIp")
+{
+    try
+    {
+        $ipRange = [Amazon.EC2.Model.IpRange]@{ CidrIp="$ip/32"; Description="$ipDescription" }
+        $ip1 = @{ IpProtocol="tcp"; FromPort="$port"; ToPort="$port"; Ipv4Ranges= $ipRange }
+        Grant-EC2SecurityGroupIngress -GroupName "$groupName" -region $region -IpPermission $ip1
+    }
+    catch [Exception]
+    {
+        Write-Host $_
+        Exit 100
+    }
+}
+elseif($option -eq "removeIp")
+{
+    try
+    {
+        $ip = (Get-EC2SecurityGroup -GroupName "$groupName" -region $region).IpPermissions | Where-Object{ $_.Ipv4Ranges.Description -eq "$ipDescription" } | ForEach-Object{
+            Revoke-EC2SecurityGroupIngress -GroupName "$groupName" -region $region -IpPermission $_
+        }
+    }
+    catch [Exception]
+    {
+        Write-Host $_
+        Exit 100
+    }
+}
+elseif($option -eq "test")
+{
+    SMA_GetAWSInstanceByTag -instancetag "$tag" -region $region    
+}
+else
+{
+    Write-Host "No -option specified!"
+    Exit 999
+}
---
+++ AWS_EC2_module.psm1
@@ -0,0 +1,301 @@
+﻿#PowerShell Module file for Amazon Web Services
+
+function SMA_AWSVersion
+{
+    Write-Host "****************"
+    Write-Host "AWS Version 1.0"
+    Write-host "****************"
+}
+
+##################################################################################################
+#Needed to get the necessary cmdlet files
+#Install-Package -Name AWSPowerShell.NetCore -Source https://www.powershellgallery.com/api/v2/ -ProviderName NuGet -ExcludeVersion -Destination "C:\Program Files (x86)\PowerShell"
+
+#SDK Install needed for AWS PowerShell
+#https://aws.amazon.com/powershell/
+
+#Sets up profile/keys so that it doesnt need to be called each time
+function SMA_SetupAWSProfile($accesskey,$secretkey,$profile)
+{
+    try
+    {
+        Set-AWSCredential -StoreAs $profile -AccessKey $accesskey -SecretKey $secretkey
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 101
+    }
+}
+
+
+#Sets up profile/keys so that it doesnt need to be called each time
+function SMA_GetAWSProfiles()
+{
+    try
+    {
+        $profiles = Get-AWSCredential -ListProfileDetail
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 101
+    }
+
+    if($profiles.Count -eq 0)
+    {
+        Write-Host "No AWS credentials stored on this account"
+        Exit 999
+    }
+
+    return $profiles
+}
+
+#Gets an AWS instance information by tag
+function SMA_CreateAWSImage($instancetag,$imagename,$imagedescription,$region)
+{
+    try
+    {
+        $getinfo = (Get-EC2Instance -Region $region).Instances | Where-Object{$_.Tag.Value -eq $instancetag}
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 101
+    }
+
+    try
+    {
+        $newImage = New-EC2Image -InstanceId $getinfo.InstanceId -Name $imagename -Description $imagedescription -Region $region
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 102
+    }
+
+    return $newImage
+}
+
+#Stops an AWS Instance
+function SMA_StopAWSInstance($id,$region)
+{
+    try
+    {
+        $stop = Stop-EC2Instance -InstanceId $id -Region $region
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+}
+
+#Starts an AWS Instance
+function SMA_StartAWSInstance($id,$region)
+{
+    try
+    {
+        $start = Start-EC2Instance -InstanceId $id -Region $region
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+}
+
+#Removes an AWS Instance
+function SMA_RemoveAWSInstance($id,$region)
+{
+    try
+    {
+        $remove = Remove-EC2Instance -InstanceId $id -Region $region -Force 
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+}
+
+#Restarts an AWS instance
+function SMA_RestartAWSInstance($id,$region)
+{
+    try
+    {
+        $restart = Restart-EC2Instance -InstanceId $id -Region $region -Force 
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+}
+
+#Gets information about an instance based off the id
+function SMA_GetAWSInstanceById($id,$region)
+{
+    try
+    {
+        $get = Get-EC2Instance -InstanceId $id -Region $region
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+    return $get
+}
+
+#Gets an AWS instance information by tag
+function SMA_GetAWSInstanceByTag($instancetag,$region)
+{
+    try
+    {
+        $get = (Get-EC2Instance -Region $region).Instances | Where-Object{($_.Tag.Key -eq "Name") -and ($_.Tag.Value -eq $instancetag)}
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+
+    return $get
+}
+
+#Gets an AWS volume information by tag
+function SMA_GetAWSVolumeByTag($tag,$region)
+{
+    try
+    {
+        $get = Get-EC2Volume -Region $region | Where-Object{($_.Tag.Key -eq "Name") -and ($_.Tag.Value -eq $tag)}
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 104
+    }
+
+    return $get
+}
+
+#Creates a new AWS instance based of input image
+function SMA_CreateAWSInstance($awsimageid,$awsimagename,$awsinstancetype,$tagvalue,$key,$region,$isblank)
+{
+    if($awsimagename)
+    {
+        if($isblank -ne "yes")
+        {
+            try
+            {
+                $awsimageid = (Get-EC2Image -Region $region -Filter @{Name="name";Values="$imagename"}).ImageId 
+            }
+            catch [Exception]
+            {
+                Write-Host "Problem getting Image Id with supplied Image Name!"
+                Write-Host $_.Exception.Message
+                Exit 102
+            }
+        }
+        else
+        {
+            try
+            {
+                $awsimageid = (Get-EC2ImageByName $awsimagename).ImageId 
+            }
+            catch [Exception]
+            {
+                Write-Host "Problem getting Image Id with supplied Image Name!"
+                Write-Host $_.Exception.Message
+                Exit 102
+            }
+        }
+    }
+    
+    try
+    {
+        $tag1 = @{ Key="Name"; Value="$tagvalue" }
+        $tag2 = @{ Key="Name"; Value="$tagvalue" }
+        $tagspec1 = New-Object Amazon.EC2.Model.TagSpecification
+        $tagspec2 = New-Object Amazon.EC2.Model.TagSpecification
+        $tagspec1.ResourceType = "instance"
+        $tagspec2.ResourceType = "volume"
+        $tagspec1.Tags.Add($tag1)
+        $tagspec2.Tags.Add($tag2)
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 102
+    }
+
+    try
+    {
+        $newinstance = New-EC2Instance -ImageId $awsimageid -MaxCount 1 -InstanceType $awsinstancetype -TagSpecification $tagspec1,$tagspec2 -KeyName $key -Region $region
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 103
+    }
+
+    Start-Sleep -s 10
+    return $newinstance
+}
+
+#Creates a new EBS volume
+function SMA_CreateAWSVolume($tag,$region,$volumetype,$volumesize)
+{
+    $tag = @{ Key="Name"; Value="$tag" }
+    $tagspec = new-object Amazon.EC2.Model.TagSpecification
+    $tagspec.ResourceType = "volume"
+    $tagspec.Tags.Add($tag)
+
+    try
+    {
+        $zone = Get-EC2AvailabilityZone -Region $region | Where-Object{ $_.State -eq "available" }
+        if($zone.Count -gt 0)
+        {
+            $zonename = $zone[0].ZoneName
+        }
+        else
+        {
+            Write-Host "No zones available for volume!"
+            Exit 101
+        }
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 100
+    }
+
+    try
+    {
+        $volume = New-EC2Volume -Size $volumesize -VolumeType $volumetype -AvailabilityZone $zonename -TagSpecification $tagspec
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 101
+    }
+
+    return $volume
+}
+
+#Creates a keypair that is necessary to be able to login to AWS Instances
+function SMA_CreateAWSKeyPair($key,$region)
+{
+    try
+    {
+        $keypair = New-EC2KeyPair -KeyName $key -Region $region
+    }
+    catch [Exception]
+    {
+        Write-Host $_.Exception.Message
+        Exit 101
+    }
+
+    return $keypair
+}
